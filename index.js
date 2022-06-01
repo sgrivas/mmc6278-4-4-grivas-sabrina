7776270699ef2a248297c1db600c00d57e14533f
Sabrina Grivas <sgrivas1@gmail.com>
2022-06-01 08:07:02 -0400

    pick a random word script

--- index.js
+++ index.js
@@ -11,3 +11,20 @@ var words = [
   'ukulele',
   'mango'
 ]
+
+var wordToGuessEl=document.getElementById('word-to-guess');
+var previousWordEl=document.getElementById('previous-word');
+var incorrectLettersEl=document.getElementById('incorrect-letters');
+var remainingGuessesEl=document.getElementById('remaining-guesses');
+var winsEl=document.getElementById('wins');
+var lossesEl=document.getElementById('losses');
+
+//Pick a random word, replace characters with underscore, show hidden word
+var randomWord=words[Math.floor(Math.random() * words.length)];
+wordToGuessEl.textContent=randomWord.replace(/[a-z]/g, '_');
+
+var guessCount=10
+remainingGuessesEl.textContent=guessCount;
+
+
+
